a4738245a7f9ef16952dbfb2ae5c2c546af9b9f3
Roshan Nair <roshannair89@gmail.com>
2025-12-17 00:45:24 -0700

    updated basetest to use ChromeOptions for headless runs

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,28 +4,39 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 
-    protected WebDriver driver;
-    protected WebDriverWait wait;
+	protected WebDriver driver;
+	protected WebDriverWait wait;
 
-    @BeforeMethod
-    public void setUp() {
-        driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+	@BeforeMethod
+	public void setUp() {
+		WebDriverManager.chromedriver().setup();
 
-    }
+		// Chrome options
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless"); 
+		options.addArguments("--no-sandbox"); 
+		options.addArguments("--disable-dev-shm-usage"); 
+		options.addArguments("--disable-gpu");
+		driver = new ChromeDriver(options);
 
-    @AfterMethod
-    public void tearDown() {
-        if (driver != null) {
-            driver.quit();
-        }
-    }
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+
+	}
+
+	@AfterMethod
+	public void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+	}
 }
